ef1ec5e048b8b2b4d850f186f4d23c02859ae828
filipwisniewski491-dot <filip.wisniewski491@gmail.com>
2026-06-23 17:33:59 +0200

    Update meilisearch-client.ts

--- lib/meilisearch-client.ts
+++ lib/meilisearch-client.ts
@@ -1,7 +1,7 @@
-import { MeiliSearch } from 'meilisearch';
+import { Meilisearch } from 'meilisearch';
 
 // Inicjalizacja klienta z użyciem zmiennych z Twojego .env.local
-export const meiliClient = new MeiliSearch({
+export const meiliClient = new Meilisearch({
   host: process.env.NEXT_PUBLIC_MEILISEARCH_HOST || '',
   apiKey: process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY || '',
 });
