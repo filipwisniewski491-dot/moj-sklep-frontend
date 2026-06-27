7d48787298a43cb38e73c26933dabd3ce3763e98
filipwisniewski491-dot <filip.wisniewski491@gmail.com>
2026-06-27 13:12:15 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,28 +29,35 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://centrumrolnictwa-cdn.b-cdn.net" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://centrumrolnictwa-cdn.b-cdn.net" />
 
+        {/* Zgoda RODO - domyślnie odmowa do czasu akceptacji (Consent Mode v2) */}
         <script dangerouslySetInnerHTML={{ 
           __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('consent', 'default', { 'ad_storage': 'denied', 'ad_user_data': 'denied', 'ad_personalization': 'denied', 'analytics_storage': 'denied', 'wait_for_update': 500 });` 
         }} />
-        
+
+        {/* 🔥 UCZCIWE ładowanie GTM: po pierwszym renderze (nie blokuje LCP), bez wykrywania botów.
+            Ten sam kod dla wszystkich - prawdziwy user i narzędzia pomiarowe traktowane identycznie.
+            GTM dociąga się przy pierwszej interakcji LUB gdy przeglądarka jest bezczynna. */}
         <script dangerouslySetInnerHTML={{ __html: `
           (function() {
-            const isBot = /Lighthouse|PageSpeed|Googlebot|GTmetrix|pingdom|bot|spider|crawl/i.test(navigator.userAgent);
-            if (!isBot) {
-              let gtmLoaded = false;
-              function loadGTM() {
-                if (gtmLoaded) return;
-                gtmLoaded = true;
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NBWX4LWC');
-              }
-              window.addEventListener('scroll', loadGTM, { passive: true, once: true });
-              window.addEventListener('mousemove', loadGTM, { passive: true, once: true });
-              window.addEventListener('touchstart', loadGTM, { passive: true, once: true });
-              setTimeout(loadGTM, 3000); 
+            let gtmLoaded = false;
+            function loadGTM() {
+              if (gtmLoaded) return;
+              gtmLoaded = true;
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NBWX4LWC');
+            }
+            // Pierwsza interakcja użytkownika - pokrywa praktycznie każdą realną wizytę
+            ['scroll','mousemove','touchstart','keydown','pointerdown'].forEach(function(evt){
+              window.addEventListener(evt, loadGTM, { passive: true, once: true });
+            });
+            // Fallback dla wizyt bez interakcji - gdy przeglądarka się wyciszy po załadowaniu
+            if ('requestIdleCallback' in window) {
+              requestIdleCallback(loadGTM, { timeout: 8000 });
+            } else {
+              setTimeout(loadGTM, 8000);
             }
           })();
         `}} />
