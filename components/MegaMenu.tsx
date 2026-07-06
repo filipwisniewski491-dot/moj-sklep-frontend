fed02d0867a8b9df7fb21d9849fe138e7ef0c8f9
filipwisniewski491-dot <filip.wisniewski491@gmail.com>
2026-07-06 10:01:18 +0200

    Update MegaMenu.tsx

--- components/MegaMenu.tsx
+++ components/MegaMenu.tsx
@@ -34,26 +34,24 @@ const MEGA_MENU_DATA: MegaCategory[] = [
       {
         heading: "Sprzęt udojowy",
         links: [
-          { text: "Instalacje udojowe i rurociągi", href: P + "hodowla-i-zootechnika-instalacje-udojowe-i-rurociagi", badge: "flagowe" },
-          { text: "Dojarki bańkowe i akcesoria", href: P + "hodowla-i-zootechnika-dojarki-bankowe-i-akcesoria" },
+          { text: "Instalacje udojowe i rurociągi", href: P + "instalacje-udojowe-i-rurociagi", badge: "flagowe" },
+          { text: "Dojarki bańkowe i akcesoria", href: P + "dojarki-bankowe-i-akcesoria" },
         ],
       },
       {
         heading: "Bydło i produkcja",
         links: [
-          { text: "Artykuły dla bydła i cieląt", href: P + "hodowla-i-zootechnika-artykuly-dla-bydla-i-cielat" },
-          { text: "Poidła, karmidła i paśniki", href: P + "hodowla-i-zootechnika-poidla-karmidla-i-pasniki" },
-          { text: "Higiena, pielęgnacja, zdrowie", href: P + "hodowla-i-zootechnika-higiena-pielegnacja-zdrowie" },
-          { text: "Wyposażenie budynków inwentarskich", href: P + "hodowla-i-zootechnika-wyposazenie-budynkow-inwentarskich" },
+          { text: "Artykuły dla bydła i cieląt", href: P + "artykuly-dla-bydla-i-cielat" },
+          { text: "Poidła, karmidła i paśniki", href: P + "poidla-karmidla-i-pasniki" },
+          { text: "Higiena, pielęgnacja, zdrowie", href: P + "higiena-pielegnacja-i-zdrowie" },
+          { text: "Wyposażenie budynków inwentarskich", href: P + "wyposazenie-budynkow-inwentarskich" },
         ],
       },
       {
         heading: "Zwierzęta i wyposażenie",
         links: [
-          { text: "Konie i jeździectwo", href: P + "hodowla-i-zootechnika-artykuly-dla-koni-i-jezdziectwo" },
-          { text: "Psy, koty i inne", href: P + "hodowla-i-zootechnika-artykuly-dla-psow-i-kotow" },
-          { text: "Trzoda chlewna", href: P + "hodowla-i-zootechnika-artykuly-dla-trzody-chlewnej" },
-          { text: "Drób", href: P + "hodowla-i-zootechnika-artykuly-dla-drobiu" },
+          { text: "Konie i jeździectwo", href: P + "artykuly-dla-koni-i-jezdziectwo" },
+          { text: "Ogrodzenia elektryczne", href: P + "ogrodzenia-elektryczne" },
         ],
       },
     ],
@@ -65,26 +63,25 @@ const MEGA_MENU_DATA: MegaCategory[] = [
     columns: [
       {
         links: [
-          { text: "Kabina, szyby, karoseria", href: P + "czesci-do-ciagnikow-kabina-szyby-karoseria" },
-          { text: "Układ napędowy i sprzęgła", href: P + "czesci-do-ciagnikow-uklad-napedowy-i-sprzegla" },
-          { text: "Silnik i osprzęt", href: P + "czesci-do-ciagnikow-silnik-i-osprzet" },
-          { text: "TUZ i hydraulika ciągnikowa", href: P + "czesci-do-ciagnikow-tuz-i-hydraulika-ciagnikowa" },
+          { text: "Kabina, szyby, karoseria", href: P + "kabina-szyby-karoseria-i-silowniki-gazowe" },
+          { text: "Układ napędowy i sprzęgła", href: P + "uklad-napedowy-i-sprzegla" },
+          { text: "Silnik i osprzęt", href: P + "silnik-i-osprzet" },
+          { text: "TUZ i hydraulika ciągnikowa", href: P + "tuz-podnosnik-i-hydraulika-ciagnikowa" },
         ],
       },
       {
         links: [
-          { text: "Układ paliwowy i wydechowy", href: P + "czesci-do-ciagnikow-uklad-paliwowy-i-wydechowy" },
-          { text: "Układ chłodzenia", href: P + "czesci-do-ciagnikow-uklad-chlodzenia" },
-          { text: "Instalacja elektryczna", href: P + "czesci-do-ciagnikow-instalacja-elektryczna" },
-          { text: "Siedzenia i fotele", href: P + "czesci-do-ciagnikow-siedzenia-i-fotele" },
+          { text: "Układ paliwowy i wydechowy", href: P + "uklad-paliwowy-i-wydechowy" },
+          { text: "Układ chłodzenia", href: P + "uklad-chlodzenia" },
+          { text: "Instalacja elektryczna", href: P + "instalacja-elektryczna-i-rozruszniki" },
+          { text: "Siedzenia i fotele", href: P + "siedzenia-i-fotele" },
         ],
       },
       {
         links: [
-          { text: "Układ kierowniczy i oś", href: P + "czesci-do-ciagnikow-uklad-kierowniczy-i-os" },
-          { text: "Oświetlenie i LED", href: P + "czesci-do-ciagnikow-oswietlenie-i-led" },
-          { text: "Układ hamulcowy", href: P + "czesci-do-ciagnikow-uklad-hamulcowy" },
-          { text: "Klimatyzacja i ogrzewanie", href: P + "czesci-do-ciagnikow-klimatyzacja-i-ogrzewanie" },
+          { text: "Układ kierowniczy i oś", href: P + "uklad-kierowniczy-i-os-przednia" },
+          { text: "Oświetlenie i LED", href: P + "oswietlenie-lampy-robocze-i-led" },
+          { text: "Układ hamulcowy", href: P + "uklad-hamulcowy" },
         ],
       },
     ],
@@ -96,25 +93,24 @@ const MEGA_MENU_DATA: MegaCategory[] = [
     columns: [
       {
         links: [
-          { text: "Uprawa ziemi", href: P + "czesci-do-maszyn-uprawa-ziemi" },
-          { text: "Zbiór i żniwa", href: P + "czesci-do-maszyn-zbior-i-zniwa" },
-          { text: "Zbiór zielonki", href: P + "czesci-do-maszyn-zbior-zielonki" },
-          { text: "Maszyny komunalne i mulczery", href: P + "czesci-do-maszyn-maszyny-komunalne-i-mulczery" },
+          { text: "Uprawa ziemi", href: P + "uprawa-ziemi" },
+          { text: "Zbiór i żniwa", href: P + "zbior-i-zniwa" },
+          { text: "Zbiór zielonki", href: P + "zbior-zielonki" },
+          { text: "Maszyny komunalne i mulczery", href: P + "maszyny-komunalne-i-mulczery" },
         ],
       },
       {
         links: [
-          { text: "Ładowacze czołowe (TUR)", href: P + "czesci-do-maszyn-ladowacze-czolowe-tur" },
-          { text: "Siew i sadzenie", href: P + "czesci-do-maszyn-siew-i-sadzenie" },
-          { text: "Wycinaki i ładowacze kiszonki", href: P + "czesci-do-maszyn-wycinaki-i-ladowacze-kiszonki" },
-          { text: "Beczkowozy i rozrzutniki", href: P + "czesci-do-maszyn-beczkowozy-i-rozrzutniki" },
+          { text: "Ładowacze czołowe (TUR)", href: P + "ladowacze-czolowe-tur-i-osprzet" },
+          { text: "Siew i sadzenie", href: P + "siew-i-sadzenie" },
+          { text: "Wycinaki i ładowacze kiszonki", href: P + "wycinaki-do-kiszonki-i-ladowacze" },
+          { text: "Beczkowozy i rozrzutniki", href: P + "beczkowozy-asenizacja-i-rozrzutniki" },
         ],
       },
       {
         links: [
-          { text: "Ochrona roślin i nawożenie", href: P + "czesci-do-maszyn-ochrona-roslin-i-nawozenie" },
-          { text: "Wozy paszowe", href: P + "czesci-do-maszyn-wozy-paszowe" },
-          { text: "Transport rolniczy", href: P + "czesci-do-maszyn-transport-rolniczy" },
+          { text: "Ochrona roślin i nawożenie", href: P + "ochrona-roslin-i-nawozenie" },
+          { text: "Wozy paszowe", href: P + "wozy-paszowe-i-technika-paszowa" },
         ],
       },
     ],
@@ -126,14 +122,13 @@ const MEGA_MENU_DATA: MegaCategory[] = [
     columns: [
       {
         links: [
-          { text: "Pneumatyka rolnicza i złącza", href: P + "czesci-do-ciagnikow-i-maszyn-pneumatyka-rolnicza-i-zlacza" },
-          { text: "Zaczepy, dyszle, technika sprzęgu", href: P + "czesci-do-ciagnikow-i-maszyn-zaczepy-dyszle-technika-sprzegu" },
+          { text: "Pneumatyka rolnicza i złącza", href: P + "pneumatyka-rolnicza-i-zlacza" },
+          { text: "Zaczepy, dyszle, technika sprzęgu", href: P + "zaczepy-dyszle-i-technika-sprzegu" },
         ],
       },
       {
         links: [
-          { text: "Oświetlenie ostrzegawcze i znakowanie", href: P + "czesci-do-ciagnikow-i-maszyn-oswietlenie-ostrzegawcze-i-znakowanie" },
-          { text: "Opony, felgi, dętki", href: P + "czesci-do-ciagnikow-i-maszyn-opony-felgi-detki" },
+          { text: "Oświetlenie ostrzegawcze i znakowanie", href: P + "oswietlenie-ostrzegawcze-i-znakowanie" },
         ],
       },
     ],
@@ -145,20 +140,20 @@ const MEGA_MENU_DATA: MegaCategory[] = [
     columns: [
       {
         links: [
-          { text: "Szybkozłącza i złącza gwintowane", href: P + "hydraulika-silowa-szybkozlacza-i-zlacza-gwintowane", badge: "bestseller" },
-          { text: "Siłowniki hydrauliczne", href: P + "hydraulika-silowa-silowniki-hydrauliczne" },
+          { text: "Szybkozłącza i złącza gwintowane", href: P + "szybkozlacza-i-zlacza-gwintowane", badge: "bestseller" },
+          { text: "Siłowniki hydrauliczne", href: P + "silowniki-hydrauliczne" },
         ],
       },
       {
         links: [
-          { text: "Pompy, silniki, hydroakumulatory", href: P + "hydraulika-silowa-pompy-silniki-i-hydroakumulatory" },
-          { text: "Rozdzielacze i zawory", href: P + "hydraulika-silowa-rozdzielacze-i-zawory" },
+          { text: "Pompy, silniki, hydroakumulatory", href: P + "pompy-silniki-i-hydroakumulatory" },
+          { text: "Rozdzielacze i zawory", href: P + "rozdzielacze-i-zawory" },
         ],
       },
       {
         links: [
-          { text: "Akcesoria i filtry hydrauliczne", href: P + "hydraulika-silowa-akcesoria-i-filtry-hydrauliczne" },
-          { text: "Przewody, węże i rury", href: P + "hydraulika-silowa-przewody-weze-i-rury" },
+          { text: "Akcesoria i filtry hydrauliczne", href: P + "akcesoria-i-filtry-hydrauliczne" },
+          { text: "Przewody, węże i rury", href: P + "przewody-weze-i-rury" },
         ],
       },
     ],
@@ -169,28 +164,25 @@ const MEGA_MENU_DATA: MegaCategory[] = [
     href: P + "czesci-uniwersalne",
     columns: [
       {
-        heading: "Elementy złączne i montażowe",
+        heading: "Elementy złączne",
         links: [
-          { text: "Śruby i nakrętki", href: P + "czesci-uniwersalne-elementy-zlaczne-i-montazowe-sruby-i-nakretki" },
-          { text: "Pierścienie", href: P + "czesci-uniwersalne-elementy-zlaczne-i-montazowe-pierscienie" },
-          { text: "Uchwyty", href: P + "czesci-uniwersalne-elementy-zlaczne-i-montazowe-uchwyty" },
-          { text: "Osłony", href: P + "czesci-uniwersalne-elementy-zlaczne-i-montazowe-oslony" },
+          { text: "Śruby i nakrętki", href: P + "sruby-i-nakretki" },
+          { text: "Pierścienie", href: P + "pierscienie" },
+          { text: "Uchwyty", href: P + "uchwyty" },
         ],
       },
       {
         heading: "Łożyska i uszczelnienia",
         links: [
-          { text: "Talerze", href: P + "czesci-uniwersalne-lozyska-uszczelnienia-i-o-ringi-talerze" },
-          { text: "Pierścienie uszczelniające", href: P + "czesci-uniwersalne-lozyska-uszczelnienia-i-o-ringi-pierscienie-uszczelniajace" },
-          { text: "O-ringi", href: P + "czesci-uniwersalne-lozyska-uszczelnienia-i-o-ringi-o-ringi" },
+          { text: "Talerze", href: P + "talerze" },
+          { text: "Pierścienie uszczelniające", href: P + "pierscienie-uszczelniajace" },
         ],
       },
       {
-        heading: "Pasy klinowe i łańcuchy",
+        heading: "Pasy i łańcuchy",
         links: [
-          { text: "Pasy klinowe", href: P + "czesci-uniwersalne-pasy-klinowe-i-lancuchy-napedowe-pasy-klinowe" },
-          { text: "Łańcuchy", href: P + "czesci-uniwersalne-pasy-klinowe-i-lancuchy-napedowe-lancuchy" },
-          { text: "Ogniwa łańcucha", href: P + "czesci-uniwersalne-pasy-klinowe-i-lancuchy-napedowe-ogniwa-lancucha" },
+          { text: "Pasy klinowe", href: P + "pasy-klinowe" },
+          { text: "Łańcuchy", href: P + "lancuchy" },
         ],
       },
     ],
@@ -230,25 +222,19 @@ export default function MegaMenu() {
         {MEGA_MENU_DATA.map((cat) => {
           const isOpen = openId === cat.id;
           return (
-            <li
-              key={cat.id}
-              className="static"
-              onMouseEnter={() => open(cat.id)}
-            >
+            <li key={cat.id} className="static" onMouseEnter={() => open(cat.id)}>
               <Link
                 href={cat.href}
                 className={
                   "flex items-center gap-1.5 px-4 py-3.5 text-[13px] font-semibold tracking-tight transition-colors " +
-                  (cat.featured
-                    ? "text-red-700 hover:text-red-800"
-                    : "text-slate-700 hover:text-slate-900") +
+                  (cat.featured ? "text-red-700 hover:text-red-800" : "text-slate-700 hover:text-slate-900") +
                   (isOpen ? " text-slate-900" : "")
                 }
                 aria-expanded={isOpen}
               >
                 {cat.title}
-                <svg className="h-3.5 w-3.5 opacity-60" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path d="M5.5 7.5L10 12l4.5-4.5" stroke="currentColor" strokeWidth="1.5" fill="none" strokeLinecap="round" strokeLinejoin="round" />
+                <svg className="h-3.5 w-3.5 opacity-60" viewBox="0 0 20 20" fill="none" aria-hidden="true">
+                  <path d="M5.5 7.5L10 12l4.5-4.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </Link>
 
@@ -261,7 +247,7 @@ export default function MegaMenu() {
                     <div className="mb-4 flex items-center justify-between">
                       <h3 className="text-[15px] font-bold text-slate-900">{cat.title}</h3>
                       <Link href={cat.href} className="text-[12px] font-semibold text-red-600 hover:text-red-700">
-                        Zobacz wszystko →
+                        Zobacz wszystko &rarr;
                       </Link>
                     </div>
                     <div className="grid grid-cols-3 gap-x-8 gap-y-6">
@@ -299,10 +285,7 @@ export default function MegaMenu() {
           );
         })}
 
-        <li
-          className="static"
-          onMouseEnter={() => open("more")}
-        >
+        <li className="static" onMouseEnter={() => open("more")}>
           <button
             type="button"
             className={
@@ -312,8 +295,8 @@ export default function MegaMenu() {
             aria-expanded={openId === "more"}
           >
             Więcej
-            <svg className="h-3.5 w-3.5 opacity-60" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path d="M5.5 7.5L10 12l4.5-4.5" stroke="currentColor" strokeWidth="1.5" fill="none" strokeLinecap="round" strokeLinejoin="round" />
+            <svg className="h-3.5 w-3.5 opacity-60" viewBox="0 0 20 20" fill="none" aria-hidden="true">
+              <path d="M5.5 7.5L10 12l4.5-4.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
           </button>
 
